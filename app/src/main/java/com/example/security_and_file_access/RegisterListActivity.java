8cb8a06622516869530f5a776f80d7092221d4af
Andrey <andrey.melnikov@basetis.com>
2019-11-03 18:41:23 +0100

    Add recyclerView

--- app/src/main/java/com/example/security_and_file_access/RegisterListActivity.java
+++ app/src/main/java/com/example/security_and_file_access/RegisterListActivity.java
@@ -1,12 +1,60 @@
 package com.example.security_and_file_access;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegisterListActivity extends AppCompatActivity {
+
+    ArrayList<RegisterModel> registerList = new ArrayList<RegisterModel>();
+    RegisterAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_list);
+        try {
+            loadData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        RecyclerView recyclerViewRegister = findViewById(R.id.recyclerViewRegister);
+        recyclerViewRegister.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RegisterAdapter( registerList);
+        recyclerViewRegister.setAdapter(adapter);
+    }
+
+    private void loadData() throws IOException {
+        RegisterModel regiserModel;
+        int idCount = 1;
+        String id = "", password = "", date = "", encodedPassword = "";
+         InputStream inputStream = getApplication().openFileInput("data.xml");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if(line.contains("id=")){
+                id = idCount + "";
+                idCount++;
+            }
+            else if (line.contains("time")) {
+                date = line;
+            }
+            else if(line.contains("text")){
+                password = line;
+            }
+            else if(line.contains("cipher_text")){
+                encodedPassword = line;
+            }
+        }
+        regiserModel = new RegisterModel(id, password,date , encodedPassword);
+        registerList.add(regiserModel);
     }
 }
